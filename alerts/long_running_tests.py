660cdb6d87c62b7afe7e0220381951245b7cdf9a
Kai Fricke <kai@anyscale.com>
2021-06-16 10:13:43 +0100

    Adjust smoke test targets

--- alerts/long_running_tests.py
+++ alerts/long_running_tests.py
@@ -17,15 +17,15 @@ def handle_result(created_on: datetime.datetime, category: str,
             "object_spilling_shuffle", "serve", "serve_failure"
     ]:
         # Core tests
-        target_update_diff = 60
+        target_update_diff = 120
 
     elif test_name in ["apex", "impala", "many_ppo", "pbt"]:
         # Tune/RLLib style tests
-        target_update_diff = 120
+        target_update_diff = 360
     else:
         return None
 
-    if last_update_diff > 60:
+    if last_update_diff > target_update_diff:
         return f"Last update to results json was too long ago " \
                f"({last_update_diff:.2f} > {target_update_diff})"
 
--- alerts/tune_tests.py
+++ alerts/tune_tests.py
@@ -11,20 +11,26 @@ def handle_result(created_on: datetime.datetime, category: str,
     msg = ""
     time_taken = results.get("time_taken", float("inf"))
     num_terminated = results.get("trial_states", {}).get("TERMINATED", 0)
+    was_smoke_test = results.get("smoke_test", False)
 
     if test_name == "long_running_large_checkpoints":
-        # Todo
+        last_update_diff = results.get("last_update_diff", float("inf"))
+        target_update_diff = 360
+
+        if last_update_diff > target_update_diff:
+            return f"Last update to results json was too long ago " \
+                   f"({last_update_diff:.2f} > {target_update_diff})"
         return None
 
-    if test_name == "bookkeeping_overhead":
+    elif test_name == "bookkeeping_overhead":
         target_terminated = 10000
         target_time = 800
     elif test_name == "durable_trainable":
         target_terminated = 16
         target_time = 600
     elif test_name == "network_overhead":
-        target_terminated = 200
-        target_time = 1000
+        target_terminated = 100 if not was_smoke_test else 20
+        target_time = 900 if not was_smoke_test else 400
     elif test_name == "result_throughput_cluster":
         target_terminated = 1000
         target_time = 120
